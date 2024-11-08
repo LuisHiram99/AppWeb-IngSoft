7c7f35a5bc46ff78c35fae4c16b9cf191b12358e
FernandoC06 <80509138+FernandoC06@users.noreply.github.com>
2024-11-07 21:19:03 -0700

    Nuevo form asignar_roles forms.py
    Agregué un form para poder asignar roles

--- mainPage/forms.py
+++ mainPage/forms.py
@@ -11,3 +11,16 @@ class ReportForm(forms.ModelForm):
     class Meta:
         model = Reportes
         fields = ['title', 'area', 'category', 'content', 'image']
+
+class AsignarRolForm(forms.ModelForm):
+    class Meta:
+        model = Miembro
+        fields = ['rol']
+        labels = {'rol': 'Rol del miembro'}
+        widgets = {
+            'rol': forms.Select(choices=[
+                (1, 'Administrador'),
+                (2, 'Gestor'),
+                (3, 'Programador'),
+            ]),
+        }
