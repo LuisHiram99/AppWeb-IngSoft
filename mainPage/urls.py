dc0eee73ba6d4d3f153e405959ef05568460c77c
Maru <cesarmvillalobos@hotmail.com>
2024-11-28 00:20:24 -0700

    Integración lenika y cesar

--- mainPage/urls.py
+++ mainPage/urls.py
@@ -18,10 +18,19 @@ urlpatterns = [
     #URLS relacionadas con el calendario:
     path('calendario/', views.calendario, name='calendario'),
 
+    #URLS relacionadas con los riesgos
+    path('riesgos/', views.riesgosProyecto, name='riesgos'),
+    path('agregarRiesgo/<int:id_proyecto>/', views.agregarRiesgo, name='agregarRiesgo'),
+    path('eliminarRiesgo/<int:id_riesgo>/', views.eliminarRiesgo, name='eliminarRiesgo'),
+
     path('register/', user_views.register, name='register'),
     path('profile/', user_views.profile, name='profile'),
     path('reportes/', views.Report, name='reportes'),
     
     path('historias/',views.historias,name='historias'),
     path('outside/', views.outside, name='outside'),
+    path('proyectos/', views.lista_proyectos_usuario, name='lista_proyectos'),
+    path('proyectos/', views.lista_proyectos_usuario, name='lista_proyectos'),
+    path('proyectos/<int:proyecto_id>/', views.detalle_proyecto, name='detalle_proyecto'),
+    path('proyectos/<int:proyecto_id>/editar/', views.editar_proyecto, name='editar_proyecto'),
 ]
